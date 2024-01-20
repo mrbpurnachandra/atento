31c59940ee6132c44920516b4b0d0ad92860944e
Prakash Bhattarai <lightvelocity999@gmail.com>
2024-01-20 09:03:23 +0545

    Formatted project with default prettier config

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body
         className={cn(
           "min-h-screen bg-background text-foreground font-sans antialiased",
-          inter.variable
+          inter.variable,
         )}
       >
         {children}
